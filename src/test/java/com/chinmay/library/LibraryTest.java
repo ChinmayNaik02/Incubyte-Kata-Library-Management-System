57ab1c4f1e7dc7dbde1554600b6a27d2e83b47c0
ChinmayNaik02 <ChinmayNaik02>
2024-08-30 22:37:02 +0530

    [init] created a dummy test to ensure that everything is working fine

--- src/test/java/com/chinmay/library/LibraryTest.java
+++ src/test/java/com/chinmay/library/LibraryTest.java
@@ -1,4 +1,12 @@
 package com.chinmay.library;
 
-public class LibraryTest {
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class LibraryTest {
+
+    @Test
+    void dummyTest() {
+        assertEquals(4,2+2);
+    }
 }
